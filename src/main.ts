33a34a7a0d3e3218e3f7691f01fac7c27794fc4a
tatsiana_shulha <t.shulha@symfa.com>
2025-06-02 20:27:59 +0200

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = process.env.PORT;
+const DEFAULT_PORT = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configuration = new DocumentBuilder()
+    .setTitle('REST Service')
+    .setVersion('1.0')
+    .build();
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, configuration),
+  );
+  await app.listen(PORT || DEFAULT_PORT);
 }
 bootstrap();
