aa688e9cba92b62a4e827294c367a3723b9c95ae
tatsiana_shulha <t.shulha@symfa.com>
2025-06-08 19:56:49 +0200

    retturn back imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
-// import { TrackModule } from './track/track.module';
-// import { FavoritesModule } from './favorites/favorites.module';
-// import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { ArtistModule } from './artist/artist.module';
 import { UserModule } from './user/user.module';
-// import { AlbumModule } from './album/album.module';
+import { AlbumModule } from './album/album.module';
 import { DatabaseModule } from './db/db.module';
 
 @Module({
   imports: [
-    // TrackModule,
-    // FavoritesModule,
-    // ArtistModule,
+    TrackModule,
+    FavoritesModule,
+    ArtistModule,
     UserModule,
-    // AlbumModule,
+    AlbumModule,
     DatabaseModule,
   ],
 })
