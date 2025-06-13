341f7691a8a4a8f332e3d32dd76b97bbbb9d948e
tatsiana_shulha <t.shulha@symfa.com>
2025-06-13 14:20:51 +0200

    add access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,43 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { RefreshDto } from './refresh.dto';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
-
-  async signup(dto: AuthDto) {
-    this.userService.post(dto);
-  }
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(dto: AuthDto) {
     const user = await this.userService.getUser(dto.login);
     if (!user) throw new ForbiddenException();
+
+    const accessToken = await this.jwtService.signAsync({
+      login: user.login,
+      userId: user.id,
+    });
+
+    if (!accessToken) throw new ForbiddenException();
+
+    return { accessToken };
+  }
+
+  async signup(dto: AuthDto) {
+    const user = await this.userService.post(dto);
+
+    const accessToken = await this.jwtService.signAsync({
+      login: user.login,
+      userId: user.id,
+    });
+
+    if (!accessToken && !user) throw new ForbiddenException();
+
+    return {
+      id: user.id,
+      accessToken,
+    };
   }
 
   refresh(dto: RefreshDto) {}
