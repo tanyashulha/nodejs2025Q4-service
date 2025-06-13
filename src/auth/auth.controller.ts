e93af2f326d64897c44367356530d93b0553fd51
tatsiana_shulha <t.shulha@symfa.com>
2025-06-13 23:03:22 +0200

    add decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,21 +2,25 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthDto } from './auth.dto';
 import { RefreshDto } from './refresh.dto';
 import { AuthService } from './auth.service';
+import { Public } from 'src/decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('login')
   login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
   }
 
+  @Public()
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
+  @Public()
   @Post('refresh')
   refresh(@Body() dto: RefreshDto) {
     return this.authService.refresh(dto);
